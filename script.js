5b7a95b7296cc8e8687e439a38e5c931018bca69
Suhail Abdi <suhailabdi857@gmail.com>
2025-06-22 17:51:48 +0300

    Finished updating the CRUD functionality The app can now create, read, update, and delete items in the DOM It also stores the items in local storage

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 const addButton= document.querySelector('.add-task-button');
 const taskInput = document.querySelector('#new-task');
 const taskList= document.querySelector('.task-list');
-
+let tasks=[];
+let count =0;
 function addTask(){
     const li = document.createElement("li");
     const deleteButton= document.createElement("button");
     const checkbox= document.createElement("input");
+    checkbox.setAttribute("id","completed");
     checkbox.setAttribute("type","checkbox");
     deleteButton.textContent="🗑️";
     const span= document.createElement("span");
+    const p = document.createElement("p");
     li.appendChild(span);
-    span.appendChild(checkbox);
-    span.textContent= taskInput.value.trim(); 
-    console.log(taskInput.value);
+    p.textContent= taskInput.value.trim();
+    span.appendChild(p);
+    span.appendChild(checkbox); 
+    console.log(tasks);
     taskList.appendChild(li);
     span.appendChild(deleteButton);
+    tasks.push({
+        id:count,
+        text:taskInput.value,
+        completed:false
+    }   
+    )
+    checkbox.addEventListener("change",function(e){
+        if(tasks.at(li.getAttribute("count")).completed){
+            p.style.textDecoration="none";
+            p.style.fontWeight="normal";
+            tasks.at(li.getAttribute("count")).completed=false;
+            console.log(li.getAttribute("count"));
+        }else{
+            tasks.at(li.getAttribute("count")).completed=true;
+            p.style.textDecoration="line-through";
+            p.style.fontWeight="100";
+        }
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+        console.log(tasks);
+    })
+    li.setAttribute("count",count);
+    count++;
+    taskInput.value="";
+    localStorage.setItem("tasks",JSON.stringify(tasks));
     deleteButton.addEventListener("click",function(){
+        tasks=tasks.filter(item => item.id != li.getAttribute("count"));
         li.remove();
+        console.log(tasks);
         taskInput.value="";
 
+        localStorage.setItem("tasks",JSON.stringify(tasks));    
+    })
+    window.addEventListener("DOMContentLoaded",function(){
+        tasks=JSON.parse(this.localStorage.getItem("tasks"));
+        tasks.map(task => taskList.innerHTML=`
+            <li count=${task.id}><span><p>${task.text}</p><input type="checkbox"><button>🗑️</button></span></li>
+            `).join("");
+        console.log("window loaded");
     })
 }
-addButton.addEventListener("click",addTask);
+addButton.addEventListener("click",function(e){
+    if(taskInput.value===""){
+        alert("Please Enter something in the task bar")
+    }else{
+        addTask();
+    }
+});
 taskInput.addEventListener("keypress",function(e){
     if(e.key =="Enter"){
         e.preventDefault();
